8903748280cde2b2c17b4575f26802d460f9db6c
luffshyy-06 <luffshyy@gmail.com>
2025-02-20 11:26:19 +0700

    update

--- karyawan/lib/main.dart
+++ karyawan/lib/main.dart
@@ -19,7 +19,7 @@ class MainApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Placeholder(),
+      home: MyHomePage(),
     );
   }
 }
